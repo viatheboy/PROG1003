e6973bc3336054352b23ae6aebd2e11e22e1a740
viatheboy <111581762+viatheboy@users.noreply.github.com>
2023-03-09 19:54:59 +0100

    ukesoppg

--- Oppgaver/oppg16.cpp
+++ Oppgaver/oppg16.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <string>
 #include "LesData2.h"
-#include 
+#include <fstream>
+#include <iomanip>
+#include <vector>
+
 using namespace std;
 
 class Publikasjon {
     private:
         string tittel;
         float pris;
+    public: 
+        Publikasjon(ifstream & inn) {
+            inn >> pris;
+            inn.ignore();
+            getline(inn, tittel);
+        }
+        virtual void skrivTilFil(ofstream & ut) {
+            ut << fixed << setprecision(2);
+            ut << pris << ' ' << tittel << '\n';
+        }
 };
 
 class Bok : public Publikasjon {
@@ -15,16 +28,72 @@ class Bok : public Publikasjon {
         string forfatter;
         int antallSider;
     public: 
-        Publikasjon()
-};
+        Bok(ifstream & inn) : Publikasjon(inn) {
+            inn >> antallSider;
+            inn.ignore();
+            getline(inn, forfatter);
+        }
+        virtual void skrivTilFil(ofstream & ut) {
+            cout << 'B' << ' ';
+            Publikasjon::skrivTilFil(ut);
+            ut << fixed << setprecision(2);
+            ut << antallSider << " " << forfatter << "\n";
+        }
 
+};
 class Magasin : public Publikasjon {
     private:
         int aar, volum;
+    public: 
+        Magasin(ifstream & inn) : Publikasjon(inn) {
+            inn >> aar >> volum;
+            inn.ignore();
+        }
+        virtual void skrivTilFil(ofstream & ut) {
+            cout << 'M' << ' ';
+            Publikasjon::skrivTilFil(ut);
+            ut << fixed << setprecision(2);
+            ut << aar << " " << volum << "\n";
+        }
 };
 
-int main() {
+vector <Publikasjon*> gPublikasjonene;
 
+/**
+ * Hovedprogram
+ * 
+ * @return int 
+ */
+int main() {
+    ifstream innfil("oppg_16.dta");
+    ofstream utfil("ny_fil_16.DTA");
+    char objektType;
+
+    if (innfil) {
+        cout << "Leser data fra oppg_16.dta\n";
+        innfil >> objektType;
+
+        while (!innfil.eof()) {
+            innfil.ignore();
+            switch(objektType) {
+                case 'B': gPublikasjonene.push_back(new Bok(innfil));       break;
+                case 'M': gPublikasjonene.push_back(new Magasin(innfil));   break;
+                default: cout << "Finner ikke filtypen\n";
+            }
+            innfil >> objektType;
+        }
+        innfil.close();
+
+        cout << "Skriver til oppg_16.DT2\n";
+        for (int i = 0; i < gPublikasjonene.size(); i++) {
+            gPublikasjonene[i]->skrivTilFil(utfil);
+        }
+
+    } else {
+        cout << "Finner ikke filen...\n";
+    }
+
+    utfil.close();
 
     return 0;
 }
